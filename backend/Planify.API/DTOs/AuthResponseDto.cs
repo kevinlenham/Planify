10719e9f14f741fb44ac507dddfc78dfd4884296
Kevin-Le Nham <kevin.nham@hotmail.com>
2026-03-21 00:08:22 +1100

    DTOs implemented

---
+++ backend/Planify.API/DTOs/AuthResponseDto.cs
@@ -0,0 +1,11 @@
+namespace Planify.API.DTOs
+{
+    public class AuthResponseDto
+    {
+        public int UserId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Token { get; set; }
+    }
+}
